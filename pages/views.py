543aac661f432a8d16a00a5935466f15e4cc55da
unknown <Administrator@1-1.intranet.proweb.uz>
2023-04-20 14:01:33 +0500

    added templates and static files

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def home_view(request):
+    return render(request, "pages/index.html")
+
+
+def shop_view(request):
+    return HttpResponse("страница категории")
+
+def product_detail(request, slug):
+    return HttpResponse("Страница продукта")
